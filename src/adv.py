ade1af0e3864c85e504af502771072bea0580634
CDuenas <chrisaduenas@gmail.com>
2020-06-10 00:04:42 -0700

    player and room classes complete, input parser to go n/s/w/e and quit complete

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,13 +40,56 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+chris = Player("Chris", room['outside'])
+
+direction = ["n", "s", "e", "w"]
+
+
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+while True:
+
+    # * Prints the current room name
+    print(f'\n{chris.current_room.name}')
+    # * Prints the current description (the textwrap module might be useful here).
+    print(chris.current_room.description)
+    # * Waits for user input and decides what to do.
+    choice = input(
+        f"Please choose which direction to go \n(n) - Go North\n(s) - Go South\n(w) - Go West\n(e) - Go East\n(q) - Quit Game: ")
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    if choice == "n":
+        print("You moved North")
+        if chris.current_room.n_to:
+            chris.current_room = chris.current_room.n_to
+        else:
+            print("You can't move in that direction.")
+
+    elif choice == "s":
+        print("You moved South")
+        if chris.current_room.s_to:
+            chris.current_room = chris.current_room.s_to
+        else:
+            print("You can't move in that direction.")
+
+    elif choice == "e":
+        print("You moved East")
+        if chris.current_room.e_to:
+            chris.current_room = chris.current_room.e_to
+        else:
+            print("You can't move in that direction.")
+
+    elif choice == "w":
+        print("You moved West")
+        if chris.current_room.w_to:
+            chris.current_room = chris.current_room.w_to
+        else:
+            print("You can't move in that direction.")
+
+    # If the user enters "q", quit the game.
+
+    elif choice == "q":
+        print("See you next time!")
+        quit()
+
+    else:
+        print("Invalid command")
